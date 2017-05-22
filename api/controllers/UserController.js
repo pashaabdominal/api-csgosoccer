303baf46522f0a202f44d3189250ea93ebe61555
Joao Silva <joao.lucas.89@hotmail.com>
2017-05-22 23:22:51 +0100

    Add some snippet of code to user controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,21 @@
  */
 
 module.exports = {
-	
-};
 
+		getList: (req, res) => {
+			res.ok([
+				{
+					id: 1,
+					username: 'Pasha Abdominal'
+				},
+				{
+					id: 2,
+					username: 'Lucas'
+				},
+				{
+					id: 3,
+					username: 'Antonio'
+				}
+			]);
+		}
+};
