a4831e337de70b24bd5bfaec32af5a18a6ef7a8a
Afonso <afonsopoker@gmail.com>
2017-05-23 22:48:51 +0100

    Add 404 erros if JackpotEntity is null

--- api/controllers/JackpotController.js
+++ api/controllers/JackpotController.js
@@ -4,10 +4,17 @@
  * @description Server-side logic for managing jackpots
  * @help See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+
 module.exports = {
+    /**
+    * @name current
+    * @description This method returns the information about current jackpot
+    */
 		current: (req, res) => {
-			Jackpot.getCurrent()
+      Jackpot.findOneByCurrent(true)
 			.then((JackpotEntity) => {
+        if (JackpotEntity == null)
+          return res.notFound();
 				res.ok(JackpotEntity);
 			});
 		}
